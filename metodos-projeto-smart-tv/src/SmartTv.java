115d473546b6f596cff24e5868ed3e0ead2b3b1f
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-08-27 12:03:42 -0300

    Adicionando descrições sobre classes e métodos

--- metodos-projeto-smart-tv/src/SmartTv.java
+++ metodos-projeto-smart-tv/src/SmartTv.java
@@ -1,9 +1,12 @@
+// Criando uma classe: o public indica que a classe poderá ser acessada em qualquer parte do seu código, class indicaa criação da classe, e logo depois vem o nome da classe que você quer criar (lembre de usar o padrão para nomear as classes, assim: UmaNovaClasse)
 public class SmartTv {
 
     boolean ligada = false;
     int volume = 25;
     int canal = 5;
 
+
+    // os métodos são criados com o padrão de nomes camelCase
     public void ligar(){
         ligada = true;
     }
--- metodos-projeto-smart-tv/src/Usuário.java
+++ metodos-projeto-smart-tv/src/Usuário.java
@@ -2,12 +2,16 @@ public class Usuário {
     // Sempre crie um metodo main. o public significa que esse metodo podera ser usado por todas as partes do seu projeto, e void significa que esse metodo não retornará nada
 
     public static void main(String[] args) {
+        // Instanciando uma classe:
+
         // "tipo"(que no caso é o nome da classe) + nome para o objeto = new + nome da classe
         SmartTv minhSmartTv = new SmartTv();
 
         System.out.println("Minha TV está ligada? " + minhSmartTv.ligada);
         System.out.println("O volume da minha TV está em: " + minhSmartTv.volume);
 
+
+        // Depois de criar uma instancia de uma classe, você poderá acessar os módulos dela, assim:
         minhSmartTv.ligar();
 
         System.out.println("Agora minha TV está ligada? " + minhSmartTv.ligada);
