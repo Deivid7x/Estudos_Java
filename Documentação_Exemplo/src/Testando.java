a4e00c455df4e2d882d88e0ea40960ed5de1e849
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-08-28 13:17:14 -0300

    Como documentar o código em JAVA

---
+++ Documentação_Exemplo/src/Testando.java
@@ -0,0 +1,9 @@
+public class Testando {
+    public static void main(String[] args) {
+        Calculadora testando = new Calculadora();
+
+        int result = testando.somar(2, 2);
+
+        System.out.println(result);
+    }
+}
