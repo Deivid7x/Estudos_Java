164d252d93307fd885b58704abd11e6bf112b793
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-09-01 16:13:35 -0300

    For no JAVA

---
+++ Estruturas de repetição/src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        for (int gols = 1; gols <= 7; gols ++){
+            System.out.println("Gol da alemanha! A alemanha está com " + gols + "gols!");
+        }
+    }
+    
+}
