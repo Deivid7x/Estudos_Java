44423758f0be79dff1513a63ddb79d4c61b50ae5
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-09-10 18:35:45 -0300

    Getters and Setters

---
+++ Getters and Setters/src/pacote/Exemplo.java
@@ -0,0 +1,18 @@
+package pacote;
+
+public class Exemplo {
+    // Os atributos são definidos como privados, e para ter acesso a eles usamos o metodo get, e para modifica-lo usamos o método set
+    private int numero;
+
+    // Getter - depois de criar os atributos, se você digitar get, a IDE já vai sugerir a criação do método get para você
+    public int getNumero() {
+        return numero;
+    }
+
+    // Setter - de forma semelhante ao get, depois de criar o atributo e digitar set, a IDE já vai sugerir a criação do método set
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    
+}
